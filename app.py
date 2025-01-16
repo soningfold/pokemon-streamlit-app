5022bc547214decc560119e648e656dcf8c46af0
tungstennn <aqorane77@gmail.com>
2025-01-16 13:16:49 +0000

    Image API implemented

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import requests
 
 df = pd.read_csv('pokemon.csv')
 
@@ -16,3 +17,12 @@ if details.iloc[0]['type_number'] == 1:
     st.write(details[['name', 'height_m', 'weight_kg', 'type_1']])
 else:
     st.write(details[['name', 'height_m', 'weight_kg', 'type_1', 'type_2']])
+    
+# call the pokeapi to get the image of the pokemon
+response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon_number}")
+
+if response.status_code == 200:
+    image_url = response.json()['sprites']['front_default']
+    st.image(image_url, caption=f"{details['name'].iloc[0]}", use_container_width=True)
+    
+    
